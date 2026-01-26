6d6fbb612dfb2ac857571ba973977cb78dcf0723
itogakuto <drycurry4444@gmail.com>
2026-01-26 22:22:10 +0900

    change image

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -22,7 +22,7 @@ export const Home: React.FC = () => {
   
   const [heroImages, setHeroImages] = useState<string[]>([
     "images/hero-images/1.jpg",
-    "images/hero-images/2.jpg",
+    "images/hero-images/2.JPG",
     "images/hero-images/3.PNG",
     "images/hero-images/4.jpg",
     "images/hero-images/5.png",
